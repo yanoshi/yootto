aa02d2e6a84d5c6a1e475261a93720c6e1d1fb39
yanoshi <googreimu@gmail.com>
2021-01-11 14:58:18 +0900

    [Add] License

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(
         ]
     },
     classifiers=[
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ]
 )
